d7269dd82a8d0024bdbc487b2f903dfabbbafdad
kdelehoidin <kdelehoidin@exadel.com>
2024-10-28 13:02:58 +0200

    add surefire plugin

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -7,5 +7,6 @@ public class FirstTest {
     @Test
     public void test() {
         System.out.println("Hello world!");
+        System.out.println("Env is: " + System.getProperty("env"));
     }
 }
