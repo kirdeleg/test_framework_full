aeb5f98d17411acfb676c9fc5ef6d7b2822771d5
kdelehoidin <kdelehoidin@exadel.com>
2024-10-28 12:21:10 +0200

    Initial setup

--- src/test/java/utils/ConfigLoader.java
+++ src/test/java/utils/ConfigLoader.java
@@ -8,16 +8,32 @@ public class ConfigLoader {
     private static Properties properties = new Properties();
 
     static {
+        String env = System.getProperty("env");
+        if (env == null) {
+            env = System.getenv("ENV");
+        }
+        if (env == null) {
+            env = "dev";
+        }
         try {
-            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            System.out.println("Using configs for env: " + env);
+            FileInputStream fis = new FileInputStream("src/test/resources/config-" + env + ".properties");
             properties.load(fis);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Unable to use config file for env: " + env, e);
         }
     }
 
     public static String getProperty(String key) {
-        return properties.getProperty(key);
+        String value = properties.getProperty(key);
+        if (value != null) {
+            return value;
+        } else {
+            throw new RuntimeException("Key '" + key + "' was not found in config file");
+        }
     }
 
+    public static void main(String[] args) {
+        System.out.println(ConfigLoader.getProperty("base.url"));
+    }
 }
